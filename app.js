1e4123832fb99e670f2018e0e5a1b10a12b8f723
Warit Santaputra <waritsan@gmail.com>
2018-12-12 20:11:29 +0700

    Use path.join

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var electron = require('electron')  
-       
-electron.app.on('ready', function () {  
-  var mainWindow = new electron.BrowserWindow({width: 600, height: 800})  
-  mainWindow.loadURL('file://' + __dirname + '/index.html')  
+var electron = require('electron')
+var path = require('path')
+
+electron.app.on('ready', function () {
+  var mainWindow = new electron.BrowserWindow({ width: 600, height: 800 })
+  mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
 })
