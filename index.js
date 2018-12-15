71e5f22bb1208a0f306ede6d5311c67e9e4d51ca
Warit Santaputra <waritsan@gmail.com>
2018-12-15 20:55:36 +0700

    Add save pdf

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-import picture from 'cat-picture'
-import image from 'lightning-image-poly'
-
+var remote = require('electron').remote
+var fs = require('fs')
+var ImagePoly = require('lightning-image-poly')
+var picture = require('cat-picture')
 var src = picture.src
+
+// Remove the default cat picture.
 picture.remove()
-image('#visualization', null, [src], { hullAlgorithm: 'convex' })
+
+// Create the polygon editor, with the image.
+// eslint-disable-next-line no-new
+new ImagePoly('#visualization', null, [src], { hullAlgorithm: 'convex' })
+
+function save () {
+  // Electron magic to make a PDF?
+  remote.getCurrentWindow().webContents.printToPDF({
+    portrait: true
+  }, function (err, data) {
+    if (err) console.error(err)
+    // Use node file system module to create a file.
+    fs.writeFile('annotation.pdf', data, function (err) {
+      if (err) {
+        window.alert('error generating pdf! ' + err.message)
+      } else {
+        window.alert('pdf saved!')
+      }
+    })
+  })
+}
+
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode === 80) {
+    save()
+  }
+})
